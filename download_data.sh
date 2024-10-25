458d1aa014bd264035917d899f54240134c175d3
angelperedo97 <162521608+angelperedo97@users.noreply.github.com>
2024-10-25 09:58:49 -0500

    Delete download_data.sh

--- download_data.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# download and extract the CrossDocked2020 molecular structures
-wget http://bits.csb.pitt.edu/files/crossdock2020/CrossDocked2020_v1.3.tgz && \
-wget http://bits.csb.pitt.edu/files/crossdock2020/CrossDocked2020_v1.3_types.tgz && \
-mkdir CrossDocked2020 && \
-tar -xzvf CrossDocked2020_v1.3_types.tgz && \
-tar -C CrossDocked2020 -xzf CrossDocked2020_v1.3.tgz
-
